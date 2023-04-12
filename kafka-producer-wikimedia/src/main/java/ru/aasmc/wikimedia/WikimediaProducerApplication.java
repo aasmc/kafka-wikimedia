00a84ae484d2067882af9502fd6c9a6d0e7533c8
Alex Alexeyev <aasmc@yandex.ru>
2023-04-12 22:48:22 +0300

    Initial commit

---
+++ kafka-producer-wikimedia/src/main/java/ru/aasmc/wikimedia/WikimediaProducerApplication.java
@@ -0,0 +1,11 @@
+package ru.aasmc.wikimedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaProducerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WikimediaProducerApplication.class);
+    }
+}
